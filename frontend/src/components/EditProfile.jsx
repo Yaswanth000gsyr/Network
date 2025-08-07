649f52a7b7500006a89670829335c23fe8dca891
Shanmukha Yaswanth Reddy <147399502+Yaswanth000gsyr@users.noreply.github.com>
2025-08-07 14:56:18 +0530

    Update EditProfile.jsx

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://network-jgkl.onrender.com";
 
 const EditProfile = () => {
   const navigate = useNavigate();
